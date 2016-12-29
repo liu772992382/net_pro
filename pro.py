1f1f98c65c7450982ad4419bf14d4cc3f5d58dc4
gru <772992382@qq.com>
2016-12-29 09:55:07 +0800

    add auto append table

--- pro.py
+++ pro.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 from scapy.all import *
 from flask import Flask, request, render_template,redirect,make_response,flash,session,g,url_for
-from multiprocessing import Process,Value
+from multiprocessing import Process,Value, Manager
 import json
 # from json import
 
@@ -9,15 +9,19 @@ app = Flask(__name__)
 
 result = {'IP':0, 'ICMP':0, 'ARP':0, 'TCP':0, 'UDP':0}
 pat = [(IP, 'IP'), (ICMP, 'ICMP'), (ARP, 'ARP'), (TCP, 'TCP'), (UDP, 'UDP')]
-pkt_result = Value('d', 0)
+manager = Manager()
+pkt_result = manager.Value('d', 0)
+pkt_summary = manager.list()
+pkt_info = manager.list()
 
 def arp_monitor_callback(pkt):
-    global pkt_result
-    for i in range(len(pat)):
-        if pat[i][0] in pkt:
-            result[pat[i][1]] += 1
-            pkt_result.value += 1
-    # print pkt_result.value
+    # for i in range(len(pat)):
+        # if pat[i][0] in pkt:
+        #     result[pat[i][1]] += 1
+    pkt_result.value += 1
+    pkt_summary.append({'summary': pkt.summary()})
+    pkt_info.append({})
+    print pkt.summary()
 
 def p_func():
     sniff(prn=arp_monitor_callback, store=0)
@@ -30,8 +34,16 @@ def index():
 
 @app.route('/getData', methods = ['GET'])
 def getData():
-    global pkt_result
-    return json.dumps(pkt_result.value)
+    tmp = json.dumps({'result':pkt_result.value, 'summary':list(pkt_summary)})
+    print type(tmp)
+    while len(pkt_summary) != 0:
+        pkt_summary.pop()
+    return tmp
+
+
+@app.route('/test', methods = ['GET'])
+def test():
+    return render_template('test.html')
 
 
 if __name__ == "__main__":
--- static/js/table.js
+++ static/js/table.js
@@ -1,20 +1,37 @@
-$('#table_t').bootstrapTable({
-  url:'test_table',
-  striped: true,
-  clickToSelect: true,
-  columns: [{
-    field: 'id',
-    title: 'Item ID'
-    }, {
-    field: 'name',
-    title: 'Item Name'
-    }, {
-    field: 'price',
-    title: 'Item Price'
-    }]
+
+var $table = $('#table_body');
+var $table_summary = $('#table_summary');
+var $data_num = 0;
+var $button = $('#button');
+var $cors = true;
+var $data_info = [];
+$table_summary.on('click-row.bs.table', function(row, $element, field){
+
 });
 
+$button.click(function(){
+  $cors = !$cors;
+  console.log($cors);
+  if(!$cors)
+    $button.addClass('active');
+  else $button.removeClass('active');
+})
 
 setInterval(function () {
- $('#table_t').bootstrapTable('append', {id:2, name:'t', price:'s'});
-}, 1000);
+  if($cors){
+    $.get('/getData').done(function(gdata){
+      data = $.parseJSON(gdata);
+      if($data_num >= 2000){
+        $table_summary.bootstrapTable('removeAll');
+        $data_num = 0;
+      }
+      $data_num += data['summary'].length;
+      console.log(data);
+      console.log($data_num);
+      // console.log($table_summary.bootstrapTable('getScrollPosition'));
+      $table_summary.bootstrapTable('prepend', data['summary']);
+      // $table_summary.bootstrapTable('append', data['summary']);
+      // $table_summary.bootstrapTable('scrollTo', 'bottom');
+    });
+  }
+}, 2000);
